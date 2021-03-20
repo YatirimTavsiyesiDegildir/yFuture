81de26a1d68843cf2ca190435382b68e1f09afdd
Dogu <dogudeniz.ugur@gmail.com>
2021-03-21 02:21:56 +0300

    force-push to remove exposed api token

--- App.js
+++ App.js
@@ -1,68 +1,188 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the UI Kitten template
- * https://github.com/akveo/react-native-ui-kitten
- *
- * Documentation: https://akveo.github.io/react-native-ui-kitten/docs
- *
- * @format
- */
-
-import React from 'react';
-import { StyleSheet } from 'react-native';
-import {
-  ApplicationProvider,
-  Button,
-  Icon,
-  IconRegistry,
-  Layout,
-  Text,
-} from '@ui-kitten/components';
-import { EvaIconsPack } from '@ui-kitten/eva-icons';
+import React, {Component} from 'react';
+import {Alert, YellowBox} from 'react-native';
 import * as eva from '@eva-design/eva';
+import {ApplicationProvider, IconRegistry} from '@ui-kitten/components';
+import {EvaIconsPack} from '@ui-kitten/eva-icons';
+import LoginNavigator from './Screens/Navigators/LoginNavigator';
+import MainNavigator from './Screens/Navigators/MainNavigator';
+import {FetchPost} from './Utils/Fetch';
+import {StoreData, GetData} from './Utils/AsyncStorage';
+import {call} from 'react-native-reanimated';
+import theme from './src/themes/theme';
+import {client} from './back-end/OurApi';
+import {gql} from '@apollo/client';
 
-/**
- * Use any valid `name` property from eva icons (e.g `github`, or `heart-outline`)
- * https://akveo.github.io/eva-icons
- */
-const HeartIcon = (props) => (
-  <Icon {...props} name='heart'/>
-);
+YellowBox.ignoreWarnings(['Warning: ReactNative.createElement']);
 
-export default () => (
-  <>
-    <IconRegistry icons={EvaIconsPack}/>
-    <ApplicationProvider {...eva} theme={eva.light}>
-      <Layout style={styles.container}>
-        <Text style={styles.text} category='h1'>
-          Welcome to UI Kitten 😻
-        </Text>
-        <Text style={styles.text} category='s1'>
-          Start with editing App.js to configure your App
-        </Text>
-        <Text style={styles.text} appearance='hint'>
-          For example, try changing theme to Dark by using eva.dark
-        </Text>
-        <Button style={styles.likeButton} accessoryLeft={HeartIcon}>
-          LIKE
-        </Button>
-      </Layout>
-    </ApplicationProvider>
-  </>
-);
+/**GLOBALS START*/
+global.email = '';
+global.userId = 0;
+global.tckn = '';
+global.realName = '';
+global.password = '';
+global.username = '';
+global.friendsAdded = false;
+global.subscriptionWarningEnabled = false;
+/**GLOBALS END*/
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  text: {
-    textAlign: 'center',
-  },
-  likeButton: {
-    marginVertical: 16,
-  },
-});
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {isLoggedIn: false};
+  }
+
+  componentDidMount() {
+    this.checkCredentials();
+  }
+
+  clearLoginInfo() {
+    StoreData('email', '');
+    StoreData('user_id', '');
+    StoreData('tckn', '');
+    StoreData('real_name', '');
+    StoreData('password', '');
+    StoreData('username', '');
+    global.email = '';
+    global.userId = '';
+    global.tckn = '';
+    global.realName = '';
+    global.password = '';
+    global.username = '';
+    this.setState({isLoggedIn: false});
+  }
+
+  saveLoginInfo(user) {
+    StoreData('email', user.email);
+    StoreData('user_id', user.id);
+    StoreData('tckn', user.tckn);
+    StoreData('real_name', user.name);
+    StoreData('password', user.password);
+    StoreData('username', user.username);
+    global.email = user.email;
+    global.userId = user.id;
+    global.tckn = user.tckn;
+    global.realName = user.name;
+    global.password = user.password;
+    global.username = user.username;
+    this.setState({isLoggedIn: true});
+  }
+
+  async checkCredentials() {
+    let email = await GetData('email');
+    let password = await GetData('password');
+    if (
+      email !== null &&
+      email !== '' &&
+      password !== null &&
+      password !== ''
+    ) {
+      client
+        .query({
+          query: gql`
+            query MyQuery($email: String, $password: String) {
+              users(
+                where: {
+                  email: {_eq: $email}
+                  _and: {_or: {password: {_eq: $password}}}
+                }
+              ) {
+                email
+                id
+                tckn
+                username
+                name
+                password
+                phonenumber
+              }
+            }
+          `,
+          variables: {
+            email: email,
+            password: password,
+          },
+        })
+        .then(result => {
+          if (result.data.users.length === 1) {
+            let user = result.data.users[0];
+            this.saveLoginInfo(user);
+          } else {
+            this.clearLoginInfo();
+          }
+        })
+        .catch(result => {
+          this.clearLoginInfo();
+        });
+    }
+  }
+
+  logInUserWithPassword(email, password, callback) {
+    client
+      .query({
+        query: gql`
+          query MyQuery($email: String, $password: String) {
+            users(
+              where: {
+                email: {_eq: $email}
+                _and: {_or: {password: {_eq: $password}}}
+              }
+            ) {
+              email
+              id
+              tckn
+              username
+              name
+              password
+              phonenumber
+            }
+          }
+        `,
+        variables: {
+          email: email,
+          password: password,
+        },
+      })
+      .then(result => {
+        if (result.data.users.length === 1) {
+          let user = result.data.users[0];
+          this.saveLoginInfo(user);
+          callback();
+        } else {
+          Alert.alert('Email veya sifre yanlis.');
+          callback();
+        }
+      })
+      .catch(result => {
+        Alert.alert('Bir hata olustu.');
+        callback();
+      });
+  }
+
+  logout = () => {
+    this.clearLoginInfo();
+    this.setState({isLoggedIn: false});
+  };
+
+  render() {
+    return (
+      <>
+        <IconRegistry icons={EvaIconsPack} />
+        <ApplicationProvider {...eva} theme={theme.light}>
+          {this.state.isLoggedIn ? (
+            <MainNavigator
+              mainFunctions={{
+                logout: () => this.logout(),
+              }}
+            />
+          ) : (
+            <LoginNavigator
+              mainFunctions={{
+                logInUser: (email, password, callback) =>
+                  this.logInUserWithPassword(email, password, callback),
+              }}
+            />
+          )}
+        </ApplicationProvider>
+      </>
+    );
+  }
+}
