f3333868d54e536fff5d661268bb39218afa744a
Dogu <dogudeniz.ugur@gmail.com>
2021-03-21 07:03:38 +0300

    minor syntax fix

--- App.js
+++ App.js
@@ -93,7 +93,7 @@ export default class App extends Component {
                             }
                         }
                     `,
-                    variables: {user},
+                    variables: user,
                 })
                 .then(result => {
                     if (result.data.users.length === 1) {
