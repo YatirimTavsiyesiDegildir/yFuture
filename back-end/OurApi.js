57695dffdb7a190c98226d20e638c2f9e46adeda
Dogu <dogudeniz.ugur@gmail.com>
2021-03-21 06:36:00 +0300

    apollo client export fixed

--- back-end/OurApi.js
+++ back-end/OurApi.js
@@ -25,7 +25,7 @@ const authLink = setContext((_, { headers }) => {
   };
 });
 
-const client = new ApolloClient({
+export const client = new ApolloClient({
   link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
