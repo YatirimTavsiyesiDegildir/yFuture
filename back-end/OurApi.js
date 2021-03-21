2053e6c2b6390ed8d5dd43896eaa986d994503df
slariath <yunusck00@hotmail.com>
2021-03-21 06:07:14 +0300

    apollo client init at correct place

--- back-end/OurApi.js
+++ back-end/OurApi.js
@@ -6,21 +6,26 @@ import {
 import {setContext} from '../node_modules/@apollo/client/link/context';
 
 const httpLink = createHttpLink({
-  uri: 'https://ypk.hasura.app/v1/graphql',
+  uri: 'https://htf.hasura.app/v1/graphql',
 });
 
-const authLink = setContext((_, {headers}) => {
+
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token =
+      'LFpo6Qa35yULCnCfqj8iwXO6r1NIMyHftLXprNuSksV0CHQbhCrS4DjTC0JZBx5S';
   // return the headers to the context so httpLink can read them
   return {
-    headers: {
-      'content-type': 'application/json',
-      'x-hasura-admin-secret':
-        'E7v2cA2oCXG7xYcNaSeEFzOQo4p42UAGTdInXtmz4h7ZfS52XWmqKMzZq50AIEHD',
-    },
+      headers: {
+          'content-type': 'application/json',
+          'x-hasura-admin-secret':
+              'LFpo6Qa35yULCnCfqj8iwXO6r1NIMyHftLXprNuSksV0CHQbhCrS4DjTC0JZBx5S',
+      },
   };
 });
 
-export const client = new ApolloClient({
+const client = new ApolloClient({
   link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
--- index.js
+++ index.js
@@ -8,50 +8,7 @@ import { name as appName } from './app.json';
 
 import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
-import { gql } from '@apollo/client';
 
-const httpLink = createHttpLink({
-    uri: 'https://htf.hasura.app/v1/graphql',
-});
-
-
-
-const authLink = setContext((_, { headers }) => {
-    // get the authentication token from local storage if it exists
-    const token =
-        'LFpo6Qa35yULCnCfqj8iwXO6r1NIMyHftLXprNuSksV0CHQbhCrS4DjTC0JZBx5S';
-    // return the headers to the context so httpLink can read them
-    return {
-        headers: {
-            'content-type': 'application/json',
-            'x-hasura-admin-secret':
-                'LFpo6Qa35yULCnCfqj8iwXO6r1NIMyHftLXprNuSksV0CHQbhCrS4DjTC0JZBx5S',
-        },
-    };
-});
-
-const client = new ApolloClient({
-    link: authLink.concat(httpLink),
-    cache: new InMemoryCache(),
-});
-
-// const client = ...
-
-client
-    .query({
-        query: gql`
-        query MyQuery {
-  saving_target {
-    id
-    saved_value
-    target_name
-    target_value
-  }
-}
-
-      `,
-    })
-    .then(result => console.warn(result));
 
 
 
